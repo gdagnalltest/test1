d326c5c229376dac2d0e4a02865dea9cb3b8c73d
Georgi Dagnall <gdagnall@geogad.com>
2013-06-28 03:20:34 -0700

    Create lib1

--- helloworld/src/com/example/helloworld/MainActivity.java
+++ helloworld/src/com/example/helloworld/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+
+import com.example.lib1.Constants;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Log.d("Main", "Result is "+(Constants.two + Constants.two));
     }
 
 
---
+++ lib1/src/com/example/lib1/Constants.java
@@ -0,0 +1,5 @@
+package com.example.lib1;
+
+public class Constants {
+public static final int two = 2;
+}
